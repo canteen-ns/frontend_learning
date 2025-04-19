53f3c23488e66ee2e447e1be357ce40259d4918b
canteen_ns <2549076464@qq.com>
2025-04-19 20:00:24 +0800

    add js files

---
+++ javascript/index.js
@@ -0,0 +1,21 @@
+console.log("hello, javascript")
+const button = document.querySelector("button")
+button.addEventListener("click", function () {
+
+    // 写入html文件
+    // document.write(Date()); 
+
+    //写入html元素
+    // var p = document.querySelector("p");
+    // if (p.innerHTML == "hello, javascript") {
+    //     p.innerHTML = Date();
+    // }
+
+    //弹出框
+    window.alert("hello, javascript");
+
+    //控制台
+    console.log("hello, javascript");
+
+})
+
